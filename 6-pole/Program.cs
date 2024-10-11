ea81cfeb5f695500fcc110286e6e867cc27f38dc
Anony <samuel.skuta555@gmail.com>
2024-10-11 22:11:53 +0200

    add

---
+++ 6-pole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_pole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            //int[] cisla = new int[100];
+
+            int[] trpaslici = new int[7];
+
+            for (int i = 0; i < trpaslici.Length; i++)
+                trpaslici[i] = r.Next(50, 100);
+
+            int a = trpaslici[0];
+
+            for (int i = 0; i < trpaslici.Length; i++)
+                if (trpaslici[i + 1] > trpaslici[i])
+                    a = trpaslici[i];
+                    
+
+            Console.WriteLine("Najmensi trpaslik mal " + a.ToString() + "cm");
+        }
+    }
+}
---
+++ 7-pocty/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_pocty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] pocty = new int[100];
+
+            for (int i = 0; i < 10000; i++)
+            {
+                int num = r.Next(100);
+                pocty[num]++;
+            }
+            for (int i = 0; i < pocty.Length; i++)
+            {
+                Console.Write("cislo: {0}   krat: {1}\n", i, pocty[i]);
+            }
+        }
+    }
+}
