4be268eac1660019ac212080bf06e84a5a2e2517
skutas23 <skutas23@Z00P1-04>
2024-10-18 14:00:58 +0200

    func

---
+++ 9-funkcie/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_funkcie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calc("+",7,2);
+
+        }
+        static void calc(string znak, int c1, int c2)
+        {
+            int a = 0;
+            if (znak == "+")
+                a = c1 + c2;
+            else if (znak == "-")
+                a = c1 - c2;
+            else if (znak == "*")
+                a = c1 * c2;
+            else if (znak == "/")
+                a = c1 / c2;
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9.1-funkcie/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1_funkcie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* uloha spravit hru 2 hracov v hode kockou ktora moze vydat hodnotu 1-6
+              s opakovanim hodu pri cisle 6, hraci sa striedaju po hodoch a hrac vyhrava
+            pri dosiahnut skore 100 */
+
+        }
+        static int hod() 
+        {
+        Random r = new Random();
+            int c = r.Next(1, 7);
+            return c;
+        }
+        static void koc() 
+        {
+        int cc = hod();
+            if (cc == 6)
+
+        }
+    }
+}
