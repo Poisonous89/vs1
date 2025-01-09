50558b7156005ef45d7f943eda757bf6be0e22ef
Anony <samuel.skuta555@gmail.com>
2025-01-09 23:58:59 +0100

    ho

--- e12/Program.cs
+++ e12/Program.cs
@@ -13,10 +13,14 @@ namespace e12
         {
             StreamReader subor = new StreamReader("C:\\Users\\Anony\\Documents\\e12.txt");
             string[,,] p = new string[5, 1, 2];
+            var listik = new List<string> { "0", "1", "2", "3", "4" };
+            var zapis = new List<string>();
+            listik = listik.OrderBy(i => Guid.NewGuid()).ToList();
             int e = 0;
             int a = 0;
             int b = 0;
-            int i = 0;
+            int d = 0;
+            int f = 0;
             while (true)
             {
                 string obsah = subor.ReadLine();
@@ -38,18 +42,36 @@ namespace e12
                 e++;
             }
             b = 0;
-            while (i != 5)
+            /*while (d != 5)
             {
-                Console.WriteLine(p[i, 0, b]);
+                Console.WriteLine(p[d, 0, b]);
                 if (b == 0)
                     b = 1;
                 else
                 {
-                    i++;
+                    d++;
                     b = 0;
                     Console.WriteLine("");
                 }
             }
+            for (int i = 0; i < 10; i++)*/
+            while(d != 5)
+            {
+                f = Int32.Parse(listik[d]);
+                zapis.Add(p[f, 0, b]);
+                Console.WriteLine(p[f, 0, b]);
+                if (b == 0)
+                    b = 1;
+                else
+                {
+                    d++;
+                    b = 0;
+                    zapis.Add("");
+                    Console.WriteLine("");
+                }
+            }
+            subor.Close();
+            File.WriteAllLines("C:\\Users\\Anony\\Documents\\e12.txt", zapis);
             Console.ReadLine();
         }
     }
